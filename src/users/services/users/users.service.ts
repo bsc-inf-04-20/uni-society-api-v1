bea9577024cc06da03acc8aeae67c3b0a5b1089e
phillMojoo <tupomojoo@gamil.com>
2023-04-27 16:26:05 +0200

    adding an http error if it doesnt find the user on update user service method

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/typeorm/entities/user';
@@ -10,33 +10,47 @@ export class UsersService {
         @InjectRepository(User) private userRepository: Repository<User>,
     ){}
 
-    findUsers(){
+ findUsers(){
        return this.userRepository.find();
     }
 
-      async findUser(id:number){
-         const user=await this.userRepository.findOneBy({id});
-         
-         return user
+ async findUser(id:number){
+
+    const user=await this.userRepository.findOneBy({id});
+    if(user){
+         return user;}
+     else{
+        throw new HttpException({
+            status: HttpStatus.FORBIDDEN,
+            error: 'cannot find the user',
+          }, 403);
+    }
+       
       }
 
 
-    createUser(userDetails:createUserParams){
+ createUser(userDetails:createUserParams){
         const newUser=this.userRepository.create({...userDetails});
 
         return this.userRepository.save(newUser);
     }
 
-    updateUser(id:number, updateUserParams: updateUserParams){
+ updateUser(id:number, updateUserParams: updateUserParams){
 
     const thisUser=this.userRepository.findBy({id});
 
-    if(!thisUser)
-
+    if(thisUser){
      return this.userRepository.update({id},{...updateUserParams});
     }
+    else{
+        throw new HttpException({
+            status: HttpStatus.FORBIDDEN,
+            error: 'cannot find the user',
+          }, 403);
+      }
+    }
 
-    deleteUser(id:number){
+ deleteUser(id:number){
         return this.userRepository.delete({id});
     }
 
