9ecb4754999616b1a3831160a19d813e3141e664
phillMojoo <tupomojoo@gamil.com>
2023-04-28 01:47:59 +0200

    creating an authetucation guard(AuthGuard for the user controller)

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes } from '@nestjs/common';
 import { updateUserDto } from './../../dtos/updateUser.dto';
 import { createUserDto, createUserSchema } from './../../dtos/createUser.dto';
 import { UsersService } from './../../services/users/users.service';
 import { ValidateCreateUserPipe } from 'src/users/pipes/validate-create-user/validate-create-user.pipe';
+import { AuthGuard } from 'src/users/guards/auth/auth.guard';
 
 @Controller('users')
+@UseGuards(AuthGuard)
 export class UsersController {
 
     constructor(private UsersService: UsersService){}
